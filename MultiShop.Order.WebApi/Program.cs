7f87048a24770d31636183ea20c2194d00c551dd
mustafaesen <mustafaesen902@gmail.com>
2025-09-18 18:50:28 +0300

    update 1.2

--- MultiShop.Order.WebApi/Program.cs
+++ MultiShop.Order.WebApi/Program.cs
@@ -1,6 +1,50 @@
+using MultiShop.Order.Application.Features.CQRS.Handlers.AddressHandlers;
+using MultiShop.Order.Application.Features.CQRS.Handlers.OrderDetailHandlers;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Application.Services;
+using MultiShop.Order.Persistence.Context;
+using MultiShop.Order.Persistence.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          // Burada, API'nize eriþmesine izin vermek istediðiniz
+                          // frontend uygulamanýzýn adresini belirtin.
+                          // Geliþtirme için genellikle birden fazla adres ekleyebilirsiniz.
+                          policy.WithOrigins("http//localhost:1440", // React için varsayýlan
+                                             "https//localhost:1433", // Angular için varsayýlan
+                                             "https//localhost:7169") // Swagger'ýn kendisi için
+                                .AllowAnyHeader()  // Gelen istekte herhangi bir baþlýða izin ver
+                                .AllowAnyMethod(); // Herhangi bir HTTP metoduna (GET, POST, PUT, DELETE) izin ver
+                      });
+});
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddApplicationServices(builder.Configuration); 
+
+builder.Services.AddDbContext<OrderContext>();
+
+#region
+builder.Services.AddScoped<GetAddressQueryHandler>();
+builder.Services.AddScoped<GetAddressByIdQueryHandler>();
+builder.Services.AddScoped<CreateAddressCommandHandler>();
+builder.Services.AddScoped<UpdateAddressCommandHandler>();
+builder.Services.AddScoped<RemoveAddressCommandHandler>();
+
+builder.Services.AddScoped<GetOrderDetailQueryHandler>();
+builder.Services.AddScoped<GetOrderDetailByIdQueryHandler>();
+builder.Services.AddScoped<CreateOrderDetailCommandHandler>();
+builder.Services.AddScoped<UpdateOrderDetailCommandHandler>();
+builder.Services.AddScoped<RemoveOrderDetailCommandHandler>();
+#endregion
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -17,6 +61,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
